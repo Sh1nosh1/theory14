1058cbaa6eb4d9da6de76eae6f2131b1f623e700
Shinda <123310732+Sh1nosh1@users.noreply.github.com>
2024-01-15 09:15:00 +0200

    Add files via upload

---
+++ t14_1/MyClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t14_1
+{
+    /// <summary>
+    /// Класс
+    /// </summary>
+    class MyClass
+    {
+        /// <summary>
+        /// Открвтое поле
+        /// </summary>
+        public static int number;
+        /// <summary>
+        /// price
+        /// </summary>
+        public static int price;
+        /// <summary>
+        /// a
+        /// </summary>
+        public const int a = 125;
+        /// <summary>
+        /// 1. Стат. конструкт. выполн только 1 раз,вне зависимости от созданных объектов.
+        /// 2. Стат. констр. не имеет модификаторов доступа.
+        /// 3. СК никогда не имеет параметров.
+        /// 4. СК нельзя вызвать вручную, он вызывается автоматически при создании 1-го объекта.
+        /// 5. В СК, как и в стат. методе, нельзя использовать this для ссылки на текущ. объект, и можно обращаться только к статич. членам класса.
+        /// </summary>
+        static MyClass()
+        {
+            Console.WriteLine("Стат. конструктор");
+            int number = 1;
+        }
+        /// <summary>
+        /// Не статич.
+        /// </summary>
+        public MyClass()
+        {
+            Console.WriteLine("НЕстат. конструктор");
+            int number = 2;
+        }
+        /// <summary>
+        /// Вывод поля number and price
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"{number}\n{price}\n");
+        }
+        
+    }
+}
---
+++ t14_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace t14_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyClass instance1 = new MyClass();
+            //MyClass instance2 = new MyClass();
+            //instance1.number = 10;
+            //instance2.number = 20;
+            //MyClass.number = 10;
+            //MyClass.number = 20;
+            //MyClass.price = 100;
+            //instance1.Print();
+            //instance2.Print();
+            //WriteLine(MyClass.a);
+            //MyClass myClass = new MyClass();
+            //sWriteLine(MyClass.number);
+            ReadKey();
+
+        }
+    }
+}
